23068682f48ba54c4cbd02a22e0c88ca3af2e63a
Alejandro Lindarte Castro <alejandro.lindarte@globant.com>
2024-10-07 20:24:04 -0500

    apitennis first approach

--- constant.py
+++ constant.py
@@ -1,1 +1,16 @@
+import os
+
 RETURNING_STATES = {"OK": "OK", "FAIL": "FAIL"}
+
+TIME_ZONE = "America/New_York"
+
+
+API_TENNIS = {
+    "API": "https://api.api-tennis.com/tennis/",
+    "APIKEY": os.environ.get("API_TENNIS_KEY"),
+    "METHOD_GET_EVENTS": "get_events",
+    "METHOD_GET_TOURNAMENTS": "get_tournaments",
+    "METHOD_GET_FIXTURES": "get_fixtures",
+    "METHOD_GET_LIVESCORE": "get_livescore",
+    "METHOD_H2H": "get_H2H"
+}
